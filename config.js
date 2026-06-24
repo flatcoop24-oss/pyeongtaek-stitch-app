d1a66086edce1d5ca302c0fe6be7edd209d88018
flatcoop24-oss <flatcoop24@gmail.com>
2026-06-24 17:06:37 +0900

    Point Pages logo to transparent cutout asset

--- config.js
+++ config.js
@@ -1,3 +1,20 @@
 window.PYEONGTAEK_CHAEZIP_CONFIG = {
-  googleSheetWebAppUrl: "https://script.google.com/macros/s/AKfycbyboLp8QHuByzJ5OkWHciFbdPNCMWBRaro2gtkMbJpBfcHLn5OXPsHoTVzpBZNqZMTV/exec"
+  googleSheetWebAppUrl: "https://script.google.com/macros/s/AKfycbyboLp8QHuByzJ5OkWHciFbdPNCMWBRaro2gtkMbJpBfcHLn5OXPsHoTVzpBZNqZMTV/exec",
+  logoSrc: "assets/logo-chaezip-cutout-20260624.png"
 };
+
+(function () {
+  function swapLogo() {
+    var cfg = window.PYEONGTAEK_CHAEZIP_CONFIG || {};
+    var src = cfg.logoSrc || "assets/logo-chaezip-cutout-20260624.png";
+    document.querySelectorAll('img[src*="logo-chaezip"]').forEach(function (img) {
+      img.setAttribute("src", src);
+    });
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", swapLogo);
+  } else {
+    swapLogo();
+  }
+})();
